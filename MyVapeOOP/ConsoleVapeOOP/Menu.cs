2d79efbf6f557b68451a2ffd524aee4d312cd7b6
Kronborg <mkronborg7@gmail.com>
2021-05-10 14:23:14 +0200

    0.1.1
    add menu

--- MyVapeOOP/ConsoleVapeOOP/Menu.cs
+++ MyVapeOOP/ConsoleVapeOOP/Menu.cs
@@ -8,5 +8,33 @@ namespace ConsoleVapeOOP
 {
     public class Menu
     {
+        public void ListMenu()
+        {
+            try
+            {
+                Console.Clear();
+                Console.WriteLine("*******************************************");
+                Console.WriteLine("\n             Hvad vil du");
+                Console.WriteLine("\n             1. Oprette Kunder");
+                Console.WriteLine("             2. Oprette Item");
+                Console.WriteLine("             3. Oprette Ordre");
+                Console.WriteLine("             4. Slette Customer");
+                Console.WriteLine("             5. Slette Item");
+                Console.WriteLine("             6. Slette Order");
+                Console.WriteLine("             7. Alle Customer");
+                Console.WriteLine("             8. Alle Items");
+                Console.WriteLine("             9. Alle Order");
+                Console.WriteLine("             10. Finde Customer");
+                Console.WriteLine("             11. Finde Item");
+                Console.WriteLine("             12. Finde Order");
+                Console.WriteLine("\n*******************************************");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Den kunne ikke vise Menuen {0}", e);
+                throw;
+            }
+
+        }
     }
 }
